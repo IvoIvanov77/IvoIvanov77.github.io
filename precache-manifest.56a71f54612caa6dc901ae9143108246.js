aa3f3f1ed1a077d52932f677680832450e556a12
Ivaylo <ivanov_ir@abv.bg>
2018-11-25 21:02:18 +0200

    Updates

---
+++ precache-manifest.56a71f54612caa6dc901ae9143108246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b184de700242b32e467",
+    "url": "/repo_explorer/static/js/runtime~main.5b184de7.js"
+  },
+  {
+    "revision": "0b73231bc70b38c841e0",
+    "url": "/repo_explorer/static/js/main.0b73231b.chunk.js"
+  },
+  {
+    "revision": "7a28e56a65e149d7d71e",
+    "url": "/repo_explorer/static/js/1.7a28e56a.chunk.js"
+  },
+  {
+    "revision": "0b73231bc70b38c841e0",
+    "url": "/repo_explorer/static/css/main.e378a4fb.chunk.css"
+  },
+  {
+    "revision": "fcd8ac556c63bb0d474688c1ba18fa10",
+    "url": "/repo_explorer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43ec08b712bb246d4a8f774e63ba419e.js"
+  "/repo_explorer/precache-manifest.56a71f54612caa6dc901ae9143108246.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/repo_explorer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
