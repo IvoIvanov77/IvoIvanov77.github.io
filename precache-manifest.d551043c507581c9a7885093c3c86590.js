b9575bfccba3fda9ddcde7390c2a2237dfb89e67
Ivaylo <ivanov_ir@abv.bg>
2018-11-25 22:30:52 +0200

    Updates

--- precache-manifest.d551043c507581c9a7885093c3c86590.js
+++ precache-manifest.d551043c507581c9a7885093c3c86590.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/repo_explorer/static/js/runtime~main.5b184de7.js"
   },
   {
-    "revision": "34aa0ec0454dc7345210",
-    "url": "/repo_explorer/static/js/main.34aa0ec0.chunk.js"
+    "revision": "57ddff3586ea40aa9abb",
+    "url": "/repo_explorer/static/js/main.57ddff35.chunk.js"
   },
   {
     "revision": "7a28e56a65e149d7d71e",
     "url": "/repo_explorer/static/js/1.7a28e56a.chunk.js"
   },
   {
-    "revision": "34aa0ec0454dc7345210",
+    "revision": "57ddff3586ea40aa9abb",
     "url": "/repo_explorer/static/css/main.e378a4fb.chunk.css"
   },
   {
-    "revision": "12e6024e57ac55f5e19b2e8edb516c8e",
+    "revision": "807d3851f9de61e2b418c0eccf5d1009",
     "url": "/repo_explorer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/repo_explorer/precache-manifest.d31be211840981123f646e3f173c0d31.js"
+  "/repo_explorer/precache-manifest.d551043c507581c9a7885093c3c86590.js"
 );
 
 workbox.clientsClaim();
