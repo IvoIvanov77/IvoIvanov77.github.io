98a7eecce0dfd4406ce78b4b0c3611bd74fd8f02
Ivaylo <ivanov_ir@abv.bg>
2018-11-25 22:40:10 +0200

    Updates

--- precache-manifest.66c1d8d61b0d402d94571792395c074e.js
+++ precache-manifest.66c1d8d61b0d402d94571792395c074e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/repo_explorer/static/js/runtime~main.5b184de7.js"
   },
   {
-    "revision": "57ddff3586ea40aa9abb",
-    "url": "/repo_explorer/static/js/main.57ddff35.chunk.js"
+    "revision": "977efbc3ce08aa70306d",
+    "url": "/repo_explorer/static/js/main.977efbc3.chunk.js"
   },
   {
     "revision": "7a28e56a65e149d7d71e",
     "url": "/repo_explorer/static/js/1.7a28e56a.chunk.js"
   },
   {
-    "revision": "57ddff3586ea40aa9abb",
+    "revision": "977efbc3ce08aa70306d",
     "url": "/repo_explorer/static/css/main.e378a4fb.chunk.css"
   },
   {
-    "revision": "807d3851f9de61e2b418c0eccf5d1009",
+    "revision": "cee5a9489bc721546dc1b1a8b225e888",
     "url": "/repo_explorer/index.html"
   }
 ];
