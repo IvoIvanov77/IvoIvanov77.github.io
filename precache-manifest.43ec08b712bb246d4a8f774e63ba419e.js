080fc638c35fc50c241e7a42d6ac66bd09631e62
Ivaylo <ivanov_ir@abv.bg>
2018-11-25 20:27:15 +0200

    Updates

---
+++ precache-manifest.43ec08b712bb246d4a8f774e63ba419e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89dae4352ac5ce4e2171",
+    "url": "/static/js/main.89dae435.chunk.js"
+  },
+  {
+    "revision": "7a28e56a65e149d7d71e",
+    "url": "/static/js/1.7a28e56a.chunk.js"
+  },
+  {
+    "revision": "89dae4352ac5ce4e2171",
+    "url": "/static/css/main.e378a4fb.chunk.css"
+  },
+  {
+    "revision": "32534c2e5a7e4393c8761683d195ab78",
+    "url": "/index.html"
+  }
+];
